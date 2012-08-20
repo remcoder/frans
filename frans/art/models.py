79ba2f25e84c17f00aed76539b6f6a879ce8f604
Remco Veldkamp <remcoder@gmail.com>
2012-08-20 23:24:49 +0200

    art objects page can be managed from cms

--- frans/art/models.py
+++ frans/art/models.py
@@ -5,6 +5,7 @@ class ArtObject(models.Model):
   title = models.CharField(max_length=255)
   technique = models.CharField(max_length=255)
   dimensions = models.CharField(max_length=255)
+  image = models.ImageField(upload_to="art_objects")
 
   def __unicode__(self):
     return self.title
