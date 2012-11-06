0d36a9d5f015753c458f6cfcbca8edc876e875c8
Remco Veldkamp <remcoder@gmail.com>
2012-11-06 21:55:54 +0100

    slug is repopulated from title field, when adding a new column

--- frans/columns/admin.py
+++ frans/columns/admin.py
@@ -6,6 +6,7 @@ from django.contrib import admin
 class ColumnAdmin(admin.ModelAdmin):
   list_display = 'title', 'slug'
   search_fields = ['title']
-  exclude = 'slug',
+  #exclude = 'slug',
+  prepopulated_fields = {"slug": ("title",)}
 
 admin.site.register(Column, ColumnAdmin)
--- frans/columns/models.py
+++ frans/columns/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.template.defaultfilters import slugify
-
+from django.db import IntegrityError
 
 class Column(models.Model):
   title = models.CharField(max_length=255)
@@ -8,9 +8,9 @@ class Column(models.Model):
   slug = models.SlugField(max_length=255, unique=True)
 
   # override save method to generate slug from title
-  def save(self, *args, **kwargs):
-      self.slug = slugify(self.title)
-      super(Column, self).save(*args, **kwargs)
+  # def save(self, *args, **kwargs):
+  #     self.slug = slugify(self.title)
+  #     super(Column, self).save(*args, **kwargs)
 
   def __unicode__(self):
     return self.title
